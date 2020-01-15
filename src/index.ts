65dd81a3348960b0b27c66a920a00fdd9ffe5b93
unknown <rakmtube@gmail.com>
2020-01-15 21:40:35 +0900

    typechain

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import * as CryptoJS from "crypto-js"
+class Block {
+   
+    static calculateBlockHash = (index:number,previousHash:string,timestamp:number,data:string) 
+    :string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+    static vaildateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash ==="string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor(index:number,hash:string,previousHash: string,data:string,timestamp:number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+    
+}
+//Block.calculateBlockHash();
+const genesisBlock: Block = new Block(0,"20202020","","hello",123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockchain;
+
+const getLastestBlock = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data:string) : Block =>{
+    const previousBlock: Block = getLastestBlock();
+    const newIndex : number = previousBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(newIndex,previousBlock.hash,newTimestamp,data);
+    const newBlock : Block = new Block(newIndex,newHash,previousBlock.hash,data,newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+} 
+//console.log(createNewBlock("hewllo"),createNewBlock("byebye"));
+const getHashForBlock = (aBlock: Block) :string => Block.calculateBlockHash(aBlock.index,aBlock.previousHash,aBlock.timestamp,aBlock.data)
+const isBlockVaild = (candidateBlock : Block,previousBlock : Block) : boolean =>{
+    if(!Block.vaildateStructure(candidateBlock)){
+        return false;
+    }else if(previousBlock.index +1 !== candidateBlock.index){
+        return false;
+    }else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    }else if(getHashForBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    }else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock: Block) : void =>{
+    if(isBlockVaild(candidateBlock,getLastestBlock())){
+        blockchain.push(candidateBlock);
+    }
+}
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+console.log(blockchain)
+export {};
